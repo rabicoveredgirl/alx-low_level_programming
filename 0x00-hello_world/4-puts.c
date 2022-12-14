a82dab0f8623da1a64f22046a7b0d731f9a8f976
Rabiat Aliu <aliurabiat@gmail.com>
2022-12-14 13:58:17 -0800

    first c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
-int main (void)
+/* to display the follwing text using puts */
+int main(void)
 {
-	char mystr[] = "Programming is like building a multilingual puzzle";
+	char mystr[] = "\"Programming is like building a multilingual puzzle";
 	puts(mystr);
 
 	return (0);
